14a9f44b6d806b6873b85ca04f2cd2b9f80b3d3f
Disquietude <danny93ngo@gmail.com>
2018-12-28 16:19:27 -0500

    Updates

---
+++ precache-manifest.8047233b13d11e2e723fa8e5706153f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61eb5a4053f714f2194be9385aa2824d",
+    "url": "/Pomodoro-Clock/static/media/alarm.61eb5a40.mp3"
+  },
+  {
+    "revision": "0642767583402c2b9aeb",
+    "url": "/Pomodoro-Clock/static/js/runtime~main.06427675.js"
+  },
+  {
+    "revision": "63eeba5edfe43d8ce1ef",
+    "url": "/Pomodoro-Clock/static/js/main.63eeba5e.chunk.js"
+  },
+  {
+    "revision": "80a7a5fdb865c3a265ae",
+    "url": "/Pomodoro-Clock/static/js/1.80a7a5fd.chunk.js"
+  },
+  {
+    "revision": "63eeba5edfe43d8ce1ef",
+    "url": "/Pomodoro-Clock/static/css/main.683d3465.chunk.css"
+  },
+  {
+    "revision": "c016dc4b019d2325544db868b093d853",
+    "url": "/Pomodoro-Clock/index.html"
+  }
+];
